a0b109c8dcd4a25310b40d794b77244ed7d74ab3
Franck De Goër <degoerdf@ensimag.fr>
2014-12-02 08:53:32 +0100

    Importing solution for p20

---
+++ src/p20.py
@@ -0,0 +1,23 @@
+#-*- coding: utf-8 -*-
+
+import math
+
+from p import Problem
+
+class p20(Problem):
+
+    """ 
+        n! means n  (n  1)  ...  3  2  1
+
+        For example, 10! = 10  9  ...  3  2  1 = 3628800,
+        and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+        Find the sum of the digits in the number 100! 
+        
+    """
+
+    def solve(self):
+        n = 100
+        s_fact = str(math.factorial(100))
+        return sum([int(i) for i in s_fact])
+
